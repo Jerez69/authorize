723ea513405f501ad27799940e9e0e34e652c935
Marcus Wübbe <marcus.wuebbe@hotmail.de>
2024-01-29 18:00:39 +0100

    merged

--- authret/src/app/AccessTokenResponse.ts
+++ authret/src/app/AccessTokenResponse.ts
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 export class AccessTokenResponse{
     tokenType!:string;
     accessToken!:string;
     expiresIn!:number;
     refreshToken!:string;
-=======
-export class AccessTokenResponse{
-    tokenType!:string;
-    accessToken!:string;
-    expiresIn!:number;
-    refreshToken!:string;
->>>>>>> b2ad827f4b937d830b572aee8c92dd697e30ff1a
 }
--- authret/src/app/HttpValidationProblemDetails.ts
+++ authret/src/app/HttpValidationProblemDetails.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export class HttpValidationProblemDetails{
 
     public type!:string;
@@ -7,16 +6,4 @@ export class HttpValidationProblemDetails{
     public detail!:string;
     public instance!:string;
     public errors!:string[];
-
-=======
-export class HttpValidationProblemDetails{
-
-    public type!:string;
-    public title!:string;
-    public status!:number;
-    public detail!:string;
-    public instance!:string;
-    public errors!:string[];
-
->>>>>>> b2ad827f4b937d830b572aee8c92dd697e30ff1a
 }
